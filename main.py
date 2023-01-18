b566978f6028776d9bc4d19e97fd6344b8efad99
hiorws <hiorws@protonmail.com>
2023-01-18 22:57:57 +0300

    Added database and SQLAlchemy for ORM

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 app = Flask(__name__)
 
-books = [
-    [1, "Test Book#1", "Ozgur"],
-    [2, "Test Book#2", "Su"],
-    [3, "Test Book#3", "Can"],
-    [4, "Test Book#4", "Selim"],
-]
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///crt-books.db"
 
+db.init_app(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_name = db.Column(db.String, unique=True, nullable=False)
+    author_name = db.Column(db.String, unique=False, nullable=True)
+    book_photo = db.Column(db.String, unique=False, nullable=True)
+
+@app.route("/create_database")
+def create_database():
+    with app.app_context():
+        db.create_all()
+    return "DB Created."
 
 @app.route("/")
+@app.route("/home")
 def home():
     return render_template("index.html")
 
 @app.route("/books")
 def get_all_books():
+    books = Book.query.all()
     return render_template("books.html", all_books=books)
 
 @app.route("/create", methods=["GET", "POST"])
@@ -26,11 +39,14 @@ def create_book():
         book_name = request.form['book_name']
         author_name = request.form['author_name']
         
-        book_index = books[-1][0] + 1
-        books.append([book_index, book_name, author_name])
+        book = Book(
+            book_name=book_name,
+            author_name=author_name
+        )
 
-        print("BOOK_NAME: " + book_name)
-        return "Book saved."
+        db.session.add(book)
+        db.session.commit()
+        return redirect(url_for("get_all_books"))
 
 
 @app.route("/health")
